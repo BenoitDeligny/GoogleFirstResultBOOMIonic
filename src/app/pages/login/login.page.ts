f7f72d83c1e7f898fccba70a637511fea35fd27d
mydnac <mandycoelho5@gmail.com>
2020-06-25 17:36:50 +0200

    feat(login):ngmodel mail and password

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,14 @@ export class LoginPage implements OnInit {
 
   constructor(private router: Router) { }
 
+  user = { mail: '', password: '' };
+
   ngOnInit() {
   }
 
   navigateHome() {
     this.router.navigate(['/home']);
+    console.log(this.user);
   }
 
 }
