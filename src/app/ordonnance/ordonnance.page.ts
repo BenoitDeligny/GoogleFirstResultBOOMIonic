2af21245a4cb24364a92c27e40b66160fdb6b75e
couettou <delignyb.pro@gmail.com>
2020-06-28 14:28:21 +0200

    fix ordoPage

--- src/app/ordonnance/ordonnance.page.ts
+++ src/app/ordonnance/ordonnance.page.ts
@@ -5,36 +5,36 @@ import { Prescription } from '../shared/models/prescription';
 import { LocalNotifications, ELocalNotificationTriggerUnit } from '@ionic-native/local-notifications/ngx';
 
 @Component({
-  selector: 'app-ordonnance',
-  templateUrl: './ordonnance.page.html',
-  styleUrls: ['./ordonnance.page.scss'],
+    selector: 'app-ordonnance',
+    templateUrl: './ordonnance.page.html',
+    styleUrls: ['./ordonnance.page.scss'],
 })
 export class OrdonnancePage implements OnInit {
 
-  constructor(private prescriptionService: OrdonnanceServiceService, private route: ActivatedRoute, private localNotifications: LocalNotifications) { }
+    constructor(private prescriptionService: OrdonnanceServiceService, private route: ActivatedRoute, private localNotifications: LocalNotifications) { }
 
-  currentPrescription: Prescription;
-  id: number;
+    currentPrescription: Prescription;
+    id: number;
 
-  ngOnInit() {
-    this.localNotifications.schedule({
-      id: 1,
-      defaults: 1,
-      foreground: true,
-      title: 'Pense à prendre ta prescription de la journée',
-      trigger: { in: 15, unit: ELocalNotificationTriggerUnit.SECOND }
-  });
+    ngOnInit() {
+        this.localNotifications.schedule({
+            id: 1,
+            defaults: 1,
+            foreground: true,
+            title: 'Pense à prendre ta prescription de la journée',
+            trigger: { in: 15, unit: ELocalNotificationTriggerUnit.SECOND }
+        });
 
-    this.route.paramMap.subscribe(params => {
-      this.id = parseInt(params.get('id'), 10);
-    });
+        this.route.paramMap.subscribe(params => {
+            this.id = parseInt(params.get('id'), 10);
+        });
 
-    this.prescriptionService.getPrescriptionById(this.id).subscribe(data => {
-      for (const drug of data.drugs) {
-        drug.moments.sort();
-      }
-      this.currentPrescription = data;
-    });
+        this.prescriptionService.getPrescriptionById(this.id).subscribe(data => {
+            for (const drug of data.drugs) {
+                drug.moments.sort();
+            }
+            this.currentPrescription = data;
+        });
 
-  }
+    }
 }
